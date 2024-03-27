6b75fee667975c0a42b87a0a4d4eb002fd9c108b
Mohamed Abdirizak Hassan <108422509+MoShaab@users.noreply.github.com>
2024-03-27 20:46:42 +0300

    Update oop.py

--- oop.py
+++ oop.py
@@ -1,11 +1,11 @@
 class Person:
     def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender= gender
+        self.Name = name
+        self.Age = age
+        self.Gender= gender
 
     def introduce(self):
-        print('My name is', self.name, ', I am', self.age, 'years old and I am ', self.gender)
+        print('My name is', self.Name, ', I am', self.Age, 'years old and I am ', self.Gender)
 
 
 pers1 = Person("Mohamed", 28, "Male")
